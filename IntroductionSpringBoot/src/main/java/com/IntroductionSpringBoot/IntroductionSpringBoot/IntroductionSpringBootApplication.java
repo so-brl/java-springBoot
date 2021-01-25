4ad5cff1fc99a4144421650a2594a484f8f9628a
so-brl <sonia.brunel@le-campus-numerique.fr>
2021-01-25 11:58:15 +0100

    Un pas de plus vers les microservices : Eureka

--- IntroductionSpringBoot/src/main/java/com/IntroductionSpringBoot/IntroductionSpringBoot/IntroductionSpringBootApplication.java
+++ IntroductionSpringBoot/src/main/java/com/IntroductionSpringBoot/IntroductionSpringBoot/IntroductionSpringBootApplication.java
@@ -2,12 +2,23 @@ package com.IntroductionSpringBoot.IntroductionSpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableOpenApi
+@EnableDiscoveryClient
 public class IntroductionSpringBootApplication {
+
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(IntroductionSpringBootApplication.class, args);
